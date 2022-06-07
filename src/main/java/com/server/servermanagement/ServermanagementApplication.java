92064f5b5fdfc89091b2e7fa86901d41e1537bc4
Sayan <mukherjeesayan98@gmail.com>
2022-06-07 08:56:38 +0530

    first commit

---
+++ src/main/java/com/server/servermanagement/ServermanagementApplication.java
@@ -0,0 +1,13 @@
+package com.server.servermanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServermanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServermanagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/server/servermanagement/ServermanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.server.servermanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServermanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
